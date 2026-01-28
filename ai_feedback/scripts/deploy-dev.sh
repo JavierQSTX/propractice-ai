28a7648ae76744f3fa796c37f3063b152be41280
filipsz <filip.szmid@gmail.com>
2026-01-28 14:39:05 +0100

    closes #1

---
+++ ai_feedback/scripts/deploy-dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source .env
+
+gcloud run deploy \
+	my-fastapi-service-dev \
+  --project static-bond-177302 \
+	--source . \
+	--platform managed \
+	--region us-central1 \
+	--set-env-vars AI_API_KEY=$AI_API_KEY,LANGFUSE_SECRET_KEY=$LANGFUSE_SECRET_KEY,LANGFUSE_PUBLIC_KEY=$LANGFUSE_PUBLIC_KEY,LANGFUSE_HOST=$LANGFUSE_HOST,LOGIN_USERNAME=$LOGIN_USERNAME,LOGIN_PASSWORD=$LOGIN_PASSWORD,JWT_SECRET_KEY=$JWT_SECRET_KEY \
+	--allow-unauthenticated
