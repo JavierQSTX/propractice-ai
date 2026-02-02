b8c15bf0d1108c3b19628b731027276e85190236
filip <filip@gmail.com>
2026-02-02 21:34:07 +0100

    change model name to gemini-3-flash-preview

--- ai_feedback/ai_feedback/config.py
+++ ai_feedback/ai_feedback/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     ai_api_key: str
     ai_base_url: str = "https://generativelanguage.googleapis.com/v1beta/openai/"
     # ai_model_name: str = "gemini-2.5-flash-lite"
-    ai_model_name: str = "gemini-2.0-flash"
+    ai_model_name: str = "gemini-3-flash-preview"
 
     langfuse_secret_key: str
     langfuse_public_key: str
